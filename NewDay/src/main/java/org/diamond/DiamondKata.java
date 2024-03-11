70c9eaf2382852d143bbb329e88e23d54aa36968
Stephanie Tindal <sct808@cs.bham.ac.uk>
2024-03-11 15:19:10 +0000

    Add files

---
+++ NewDay/src/main/java/org/diamond/DiamondKata.java
@@ -0,0 +1,75 @@
+package org.diamond;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class DiamondKata {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Diamond Kata!");
+        DiamondKata diamondKata = new DiamondKata();
+        String diamond = diamondKata.diamondGenerator();
+        System.out.println(diamond.toString());
+    }
+
+    public String diamondGenerator(){
+        String diamond = "";
+        Scanner scanner = new Scanner(System.in);
+        String inputLetter = "";
+        while (!checkValidInput(inputLetter) && scanner.hasNext()) {
+                inputLetter = scanner.next();
+        }
+        if (checkValidInput(inputLetter)) {
+            inputLetter = inputLetter.toUpperCase();
+            System.out.println("Input letter is: " + inputLetter);
+            System.out.printf("%nDiamond:%n");
+            diamond = printDiamond(inputLetter);
+        }
+
+        return diamond;
+    }
+
+    public boolean checkValidInput(String inputLetter){
+        boolean isValidInput = false;
+        try {
+            if(inputLetter.length()!=1) {
+                System.out.printf("Please enter a single letter:%n");
+                throw new Exception("Invalid input");
+            } else if (!inputLetter.matches("[A-Za-z]")){
+                System.out.printf("Please only enter letters:%n");
+                throw new Exception("Invalid input");
+            }
+            isValidInput = true;
+        } catch (Exception e) {
+            isValidInput = false;
+        }
+        return isValidInput;
+    }
+
+    public String printDiamond(String inputLetter){
+        List<StringBuilder> diamond = new ArrayList<>();
+        int inputLetterAsInt = inputLetter.charAt(0);
+        int initialSpaceNum = (inputLetterAsInt - 'A');
+        for(int i = 0; i <= initialSpaceNum; ++i) {
+            StringBuilder diamondLine = new StringBuilder();
+            addSpaces(diamondLine, initialSpaceNum-i);
+            diamondLine.append((char)('A' + i));
+            addSpaces(diamondLine, i);
+            diamondLine.append(new StringBuilder(diamondLine.substring(0, diamondLine.length()-1)).reverse());
+            diamond.add(diamondLine);
+        }
+        for(int i = diamond.size()-2; i >= 0; --i) {
+            diamond.add(diamond.get(i));
+        }
+        return diamond.stream().map(Object::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public void addSpaces(StringBuilder diamond, int spaceNum){
+        for (int i = 0; i<spaceNum; i++){
+            diamond.append(" ");
+        }
+    }
+}
---
+++ NewDay/src/test/java/org/diamond/DiamondKataTest.java
@@ -0,0 +1,224 @@
+package org.diamond;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class DiamondKataTest {
+    private DiamondKata diamondKata = new DiamondKata();
+
+    @Test
+    public void testLetterAMinimum() {
+        String expected = new String("A");
+
+        generateUserInput("A");
+        String actual = diamondKata.diamondGenerator();
+
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+        assertEquals(expected.indexOf("A"), actual.indexOf("A"));
+        int index = expected.indexOf("A");
+        assertEquals(expected.charAt(index), actual.charAt(index));
+    }
+
+    @Test
+    public void testLetterBUppercase(){
+        String expected = new String(
+                " A \n" +
+                "B B\n" +
+                " A ");
+
+        generateUserInput("B");
+        String actual = diamondKata.diamondGenerator();
+
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+        assertEquals(expected.indexOf("B"), actual.indexOf("B"));
+        int index = expected.indexOf("B");
+        assertEquals(expected.charAt(index), actual.charAt(index));
+    }
+
+    @Test
+    public void testLetterBLowercase(){
+        String expected = new String(
+                        " A \n" +
+                        "B B\n" +
+                        " A ");
+
+        generateUserInput("b");
+
+        String actual = diamondKata.diamondGenerator();
+
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+        assertEquals(expected.indexOf("B"), actual.indexOf("B"));
+        int index = expected.indexOf("B");
+        assertEquals(expected.charAt(index), actual.charAt(index));
+    }
+
+    @Test
+    public void testLetterM() {
+        String expected = new String(
+                "            A            \n" +
+                "           B B           \n" +
+                "          C   C          \n" +
+                "         D     D         \n" +
+                "        E       E        \n" +
+                "       F         F       \n" +
+                "      G           G      \n" +
+                "     H             H     \n" +
+                "    I               I    \n" +
+                "   J                 J   \n" +
+                "  K                   K  \n" +
+                " L                     L \n" +
+                "M                       M\n" +
+                " L                     L \n" +
+                "  K                   K  \n" +
+                "   J                 J   \n" +
+                "    I               I    \n" +
+                "     H             H     \n" +
+                "      G           G      \n" +
+                "       F         F       \n" +
+                "        E       E        \n" +
+                "         D     D         \n" +
+                "          C   C          \n" +
+                "           B B           \n" +
+                "            A            ");
+
+        generateUserInput("m");
+        String actual = diamondKata.diamondGenerator();
+
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+    }
+
+    @Test
+    public void testLetterZMaximum() {
+        String expected = new String(
+                        "                         A                         \n" +
+                        "                        B B                        \n" +
+                        "                       C   C                       \n" +
+                        "                      D     D                      \n" +
+                        "                     E       E                     \n" +
+                        "                    F         F                    \n" +
+                        "                   G           G                   \n" +
+                        "                  H             H                  \n" +
+                        "                 I               I                 \n" +
+                        "                J                 J                \n" +
+                        "               K                   K               \n" +
+                        "              L                     L              \n" +
+                        "             M                       M             \n" +
+                        "            N                         N            \n" +
+                        "           O                           O           \n" +
+                        "          P                             P          \n" +
+                        "         Q                               Q         \n" +
+                        "        R                                 R        \n" +
+                        "       S                                   S       \n" +
+                        "      T                                     T      \n" +
+                        "     U                                       U     \n" +
+                        "    V                                         V    \n" +
+                        "   W                                           W   \n" +
+                        "  X                                             X  \n" +
+                        " Y                                               Y \n" +
+                        "Z                                                 Z\n" +
+                        " Y                                               Y \n" +
+                        "  X                                             X  \n" +
+                        "   W                                           W   \n" +
+                        "    V                                         V    \n" +
+                        "     U                                       U     \n" +
+                        "      T                                     T      \n" +
+                        "       S                                   S       \n" +
+                        "        R                                 R        \n" +
+                        "         Q                               Q         \n" +
+                        "          P                             P          \n" +
+                        "           O                           O           \n" +
+                        "            N                         N            \n" +
+                        "             M                       M             \n" +
+                        "              L                     L              \n" +
+                        "               K                   K               \n" +
+                        "                J                 J                \n" +
+                        "                 I               I                 \n" +
+                        "                  H             H                  \n" +
+                        "                   G           G                   \n" +
+                        "                    F         F                    \n" +
+                        "                     E       E                     \n" +
+                        "                      D     D                      \n" +
+                        "                       C   C                       \n" +
+                        "                        B B                        \n" +
+                        "                         A                         ");
+
+        generateUserInput("z");
+        String actual = diamondKata.diamondGenerator();
+
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+    }
+
+    @Test
+    public void testEmptyInput() {
+        String expected = new String();
+
+        generateUserInput("");
+        ByteArrayOutputStream outputContent = recordOutput();
+        String actual = diamondKata.diamondGenerator();
+
+        assertTrue(outputContent.toString().contains("Please enter a single letter"));
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+    }
+
+    @Test
+    public void testIncorrectInputMultipleValues() {
+        String expected = new String();
+
+        generateUserInput(String.format("aaa", System.lineSeparator()));
+        ByteArrayOutputStream outputContent = recordOutput();
+        String actual = diamondKata.diamondGenerator();
+
+        assertTrue(outputContent.toString().contains("Please enter a single letter"));
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+    }
+
+    @Test
+    public void testIncorrectInputSymbols() {
+        String expected = new String();
+
+        generateUserInput(String.format("?", System.lineSeparator()));
+        ByteArrayOutputStream outputContent = recordOutput();
+        String actual = diamondKata.diamondGenerator();
+
+        assertTrue(outputContent.toString().contains("Please only enter letters"));
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+    }
+
+    @Test
+    public void testIncorrectInputInts() {
+        String expected = new String();
+
+        generateUserInput(String.format("1", System.lineSeparator()));
+        ByteArrayOutputStream outputContent = recordOutput();
+        String actual = diamondKata.diamondGenerator();
+
+        assertTrue(outputContent.toString().contains("Please only enter letters"));
+        assertEquals(expected, actual);
+        assertEquals(expected.length(), actual.length());
+    }
+
+    public void generateUserInput(String userInput){
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+    }
+
+    public ByteArrayOutputStream recordOutput(){
+        ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputContent));
+        return outputContent;
+    }
+
+}
